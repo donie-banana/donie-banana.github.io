f5dfd6f414e7260e7a77e6ed8d328df51353a6cb
DionBeauGierman <206984@student.aventus.nl>
2024-12-16 15:59:49 +0100

    onnodige variabel picode weggehaald

--- pi-mem/scripts.js
+++ pi-mem/scripts.js
@@ -69,13 +69,12 @@ let badAlert = '';
 HSElement.innerHTML = currentHS;
 
 document.getElementById("input").addEventListener("keyup", function (event) {
-    piCode = piCurrent;
     console.log(`picode: ${piCode}, event.key: ${event.key}`)
     if (!allowedKeys.includes(event.key)) {
         return;
     }
 
-    if (event.key !== piCode) {
+    if (event.key !== piCurrent) {
         bad();
         return;
     }
